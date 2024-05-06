d999c40f3aaf622bdcdff611707c4a4414c74c5c
NanaAba08 <williamsnanaaba@gmail.com>
2024-05-06 23:24:49 +0000

    updating App.jsx with components and pages links

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/pollifylogo.jpg'
 import './App.css'
 
 function App() {
@@ -97,3 +97,4 @@ function App() {
 }
 
 export default App
+
